186c2e7fe49ec465189238db6f3dc8a97730ee77
Martin Tellez Falcón <martintellezfalcon@gmail.com>
2026-04-23 16:44:59 -0600

    feat: integracion de codigo base en HTML y JS

---
+++ frontend-web/js/api.js
@@ -0,0 +1,16 @@
+// js/api.js
+// URL base de tu API en Render
+const API_URL = 'https://edu-platform-api-a2wx.onrender.com/api';
+
+// Función global para manejar el Token en el frontend
+function getToken() {
+    return localStorage.getItem('token');
+}
+
+function setToken(token) {
+    localStorage.setItem('token', token);
+}
+
+function removeToken() {
+    localStorage.removeItem('token');
+}
